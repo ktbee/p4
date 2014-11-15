b7ffb42037f602d338f3c62350997bc87c119c5a
ktbee <kbroida@gmail.com>
2014-11-15 14:38:27 -0500

    Added routes, environment.php

--- app/routes.php
+++ app/routes.php
@@ -10,8 +10,49 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+Route::get('/get-environment',function() {
+
+    echo "Environment: ".App::environment();
+
+});
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	//return View::make('hello');
+	echo "comic home page";
+});
+
+Route::post('/', function()
+{
+	// for login form
+});
+
+Route::get('/list/{tag}', function($tag)
+{
+	echo "comic listing by tag ".$tag;
+});
+
+Route::get('/{username}/home', function($username)
+{
+	echo "This is the home of ".$username;
+});
+
+Route::get('/new', function()
+{
+	echo "create new comic here";
+});
+
+Route::post('/new', function()
+{
+	//to process new comic form
+});
+
+Route::post('/edit/{title}', function($title)
+{
+	//process form to edit comic post
+});
+
+Route::get('/{username}/list/{tag}', function($username, $tag)
+{
+	echo $username."'s comics listing by tag ".$tag;
 });
