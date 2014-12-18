0f2cf95186df8f622d55068b0d80dd7b299b90e1
ktbee <kbroida@gmail.com>
2014-12-17 21:26:14 -0500

    fixing tags

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -26,13 +26,9 @@
 					<button class="center-block btn btn-lg btn-success"><a href={{url('/comic/'.$comic->id)}}> this comic</a></button>
 					<br>
 					<p>Tags: 
-						@if(isset($comic->tag))
 						@foreach ($comic->tag as $tag)
 						  {{ $tag->name.", " }}
 						@endforeach
-						@else 
-							{{" no tags added yet"}}
-						@endif
 					</p>
 				</div>
 				<br>
